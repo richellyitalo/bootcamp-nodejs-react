7da275b279e4b1610290800960db9a3a40663c96
Richelly Italo <richellyitalo@gmail.com>
2020-01-05 18:12:29 -0300

    upload de arquivo

--- api/src/config/auth.js
+++ api/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
   secret: 'a905d89574379cec8ba1fc0438bf9597',
-  expiresIn: '1h',
+  expiresIn: '7d',
 };
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,12 @@
+import multer from 'multer';
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const router = new Router();
+const uploader = multer(multerConfig);
 
 router.post('/users', UserController.store);
 
@@ -13,5 +16,9 @@ router.post('/sessions', SessionController.store);
 router.use(authMiddleware);
 
 router.put('/users', UserController.update);
+
+router.post('/files', uploader.single('file'), (req, res) => {
+  return res.json({ message: 'ok' });
+});
 
 export default router;
