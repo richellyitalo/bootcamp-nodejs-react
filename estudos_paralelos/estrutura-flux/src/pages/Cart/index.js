e8a4b1e91077b5a4ebd8e233846be9ab9ae0a137
Richelly Italo <richellyitalo@gmail.com>
2020-01-11 04:37:57 -0300

    listagem de produtos no carro

--- estudos_paralelos/estrutura-flux/src/pages/Cart/index.js
+++ estudos_paralelos/estrutura-flux/src/pages/Cart/index.js
@@ -4,10 +4,12 @@ import {
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -21,37 +23,36 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-olympikus-attract/20/D22-1787-120/D22-1787-120_detalhe2.jpg?ims=326x"
-                alt="Tênis dahora"
-              />
-            </td>
-            <td>
-              <strong>Tênis dahora</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size={20} color="#7159c1" />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -66,3 +67,13 @@ export default function Cart() {
     </Container>
   );
 }
+
+Cart.propTypes = {
+  cart: PropTypes.shape(PropTypes.array).isRequired,
+};
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
--- estudos_paralelos/estrutura-flux/src/store/modules/cart/reducer.js
+++ estudos_paralelos/estrutura-flux/src/store/modules/cart/reducer.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = [];
 export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [...state, { ...action.product, amount: 1 }];
     default:
       return state;
   }
