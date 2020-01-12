fcd94ec00d30d7eaf821f5c46407b31a3c9e0aa1
Richelly Italo <richellyitalo@gmail.com>
2020-01-12 03:50:20 -0300

    amount evento no saga

--- estudos_paralelos/estrutura-flux/src/store/modules/cart/reducer.js
+++ estudos_paralelos/estrutura-flux/src/store/modules/cart/reducer.js
@@ -6,15 +6,8 @@ export default function(state = INITIAL_STATE, action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = state.findIndex(
-          product => product.id === action.product.id
-        );
-
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({ ...action.product, amount: 1 });
-        }
+        const { product } = action;
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
--- estudos_paralelos/estrutura-flux/src/store/modules/cart/sagas.js
+++ estudos_paralelos/estrutura-flux/src/store/modules/cart/sagas.js
@@ -1,12 +1,27 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 
 function* addToCartRequest(action) {
-  const response = yield call(api.get, `products/${action.id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === action.id)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(action.id, amount));
+  } else {
+    const response = yield call(api.get, `products/${action.id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCartRequest)]);
