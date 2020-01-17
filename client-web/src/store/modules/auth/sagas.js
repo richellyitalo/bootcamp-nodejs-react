37689962fad1577488ceaa9fa6d27fba677c1d81
Richelly Italo <richellyitalo@gmail.com>
2020-01-16 23:32:53 -0300

    loading

--- client-web/src/store/modules/auth/sagas.js
+++ client-web/src/store/modules/auth/sagas.js
@@ -1,26 +1,30 @@
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import api from '~/services/api';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { user, token } = response.data;
+    const { user, token } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não logado');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não logado');
+      return;
+    }
+
+    yield put(signInSuccess(user, token));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(user, token));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
