d117cea5c992343c242e051b6956a14acfdeb2e5
Richelly Italo <richellyitalo@gmail.com>
2020-01-19 18:59:02 -0300

    pagina profile

--- client-web/src/pages/Profile/index.js
+++ client-web/src/pages/Profile/index.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-export default function index() {
-  return <div>Profile</div>;
+import { Container } from './styles';
+
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
